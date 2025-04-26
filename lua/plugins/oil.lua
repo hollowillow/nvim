c54c9d01743384cc41896ab944f8cb8b8735e363
hollowillow <slavicekmichal@proton.me>
2025-04-26 16:48:59 +0200

    Update oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,19 +1,15 @@
 return {
-        {
+
+{
         'stevearc/oil.nvim',
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {},
-        -- Optional dependencies
-        -- dependencies = { { "echasnovski/mini.icons", opts = {}, }, },
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function ()
-                require('oil').setup({
-                        default_file_explorer = true,
-                        columns = { "icon" }, -- "permissions", "mtime", "size"
-                })
-                -- Keymaps
-                vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-        end,
-        },
+        enabled = true,
+        lazy = false,
+        cmd = "Oil",
+        keys = {{mode = 'n','-','<cmd>Oil<cr>',desc = 'Oil',},},
+        opts = { default_file_explorer = true, columns = { "icon", }, },
+},
+
+-- optional dependencies
+{'nvim-tree/nvim-web-devicons', enabled = true, lazy = false,}
+
 }
