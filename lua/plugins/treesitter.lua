56561cf04b4c87eaa93458d0799abcf589074fb2
hollowillow <slavicekmichal@proton.me>
2025-04-26 16:49:58 +0200

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,26 @@
 return {
 
 {
-"nvim-treesitter/nvim-treesitter",
-build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-end,
+        "nvim-treesitter/nvim-treesitter",
+        enabled = true,
+        lazy = false,
+        build = ":TSUpdate",
+        keys = { {mode = 'n', "<c-space>", desc = "Increment Selection"}, {mode = 'x', "<bs>", desc = "Decrement Selection",}, },
+        opts = {
+                highlight = { enable = true,},
+                indent = { enable = true,},
+                ensure_installed = {
+                        "bash",
+                        "c",
+                        "diff",
+                        "html",
+                        "markdown",
+                        "markdown_inline",
+                        "lua",
+                        "printf",
+                        "regex"
+                },
+        },
 },
 
 }
