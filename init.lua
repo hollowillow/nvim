dd8ff1d85d4bffb833b906e42fdc043a1bc4ea8b
hollowillow <slavicekmichal@proton.me>
2025-08-30 23:17:29 +0200

    Remove outdated config

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
--- default config
--- require("default.options")
--- require("default.keymaps")
--- require("default.lazy")
--- require("default.autocmds")
--- require("default.functions")
--- require("default.registers")
-
 -- minimal config
 require("minimal.options")
 require("minimal.keymaps")
--- lua/default/registers.lua
+++
@@ -1,2 +0,0 @@
--- markdown link from selection
-vim.fn.setreg("m", ":s/ /-/g\rsaW[saW[sd sd :noh\r")
